7047e6a87e181c211838a57eb7124dee6ad4ddd8
Ville <ville@pop-os.localdomain>
2022-04-11 20:43:15 +0300

    first commit

---
+++ utils/board.ts
@@ -0,0 +1,338 @@
+export const defaultBoard = [
+    [
+        {
+            tile: 'A1',
+            color: 'black',
+            unit: 'whiteTower'
+        },
+        {
+            tile: 'B1',
+            color: 'white',
+            unit: 'whiteHorse'
+        },
+        {
+            tile: 'C1',
+            color: 'black',
+            unit: 'whiteBishop'
+        },
+        {
+            tile: 'D1',
+            color: 'white',
+            unit: 'whiteQueen'
+        },
+        {
+            tile: 'E1',
+            color: 'black',
+            unit: 'whiteKing'
+        },
+        {
+            tile: 'F1',
+            color: 'white',
+            unit: 'whiteBishop'
+        },
+        {
+            tile: 'G1',
+            color: 'black',
+            unit: 'whiteHorse'
+        },
+        {
+            tile: 'H1',
+            color: 'white',
+            unit: 'whiteTower'
+        }
+    ],
+    [
+        {
+            tile: 'A2',
+            color: 'white',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'B2',
+            color: 'black',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'C2',
+            color: 'white',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'D2',
+            color: 'black',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'E2',
+            color: 'white',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'F2',
+            color: 'black',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'G2',
+            color: 'white',
+            unit: 'whitePawn'
+        },
+        {
+            tile: 'H2',
+            color: 'black',
+            unit: 'whitePawn'
+        }
+    ],
+    [
+        {
+            tile: 'A3',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'B3',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'C3',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'D3',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'E3',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'F3',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'G3',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'H3',
+            color: 'white',
+            unit: null
+        }
+    ],
+    [
+        {
+            tile: 'A4',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'B4',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'C4',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'D4',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'E4',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'F4',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'G4',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'H4',
+            color: 'black',
+            unit: null
+        }
+    ],
+    [
+        {
+            tile: 'A5',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'B5',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'C5',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'D5',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'E5',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'F5',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'G5',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'H5',
+            color: 'white',
+            unit: null
+        }
+    ],
+    [
+        {
+            tile: 'A6',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'B6',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'C6',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'D6',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'E6',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'F6',
+            color: 'black',
+            unit: null
+        },
+        {
+            tile: 'G6',
+            color: 'white',
+            unit: null
+        },
+        {
+            tile: 'H6',
+            color: 'black',
+            unit: null
+        }
+    ],
+    [
+        {
+            tile: 'A7',
+            color: 'black',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'B7',
+            color: 'white',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'C7',
+            color: 'black',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'D7',
+            color: 'white',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'E7',
+            color: 'black',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'F7',
+            color: 'white',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'G7',
+            color: 'black',
+            unit: 'blackPawn'
+        },
+        {
+            tile: 'H7',
+            color: 'white',
+            unit: 'blackPawn'
+        }
+    ],
+    [
+        {
+            tile: 'A8',
+            color: 'white',
+            unit: 'blackTower'
+        },
+        {
+            tile: 'B8',
+            color: 'black',
+            unit: 'blackHorse'
+        },
+        {
+            tile: 'C8',
+            color: 'white',
+            unit: 'blackBishop'
+        },
+        {
+            tile: 'D8',
+            color: 'black',
+            unit: 'blackQueen'
+        },
+        {
+            tile: 'E8',
+            color: 'white',
+            unit: 'blackKing'
+        },
+        {
+            tile: 'F8',
+            color: 'black',
+            unit: 'blackBishop'
+        },
+        {
+            tile: 'G8',
+            color: 'white',
+            unit: 'blackHorse'
+        },
+        {
+            tile: 'H8',
+            color: 'black',
+            unit: 'blackTower'
+        }
+    ]
+]
